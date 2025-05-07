f0e7b359e443b10ad61aae1cdd3623876d23d9e5
Anup-Naik <Anupnaik54810@gmail.com>
2025-05-07 13:42:32 +0530

    Initial 🍍🍃

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/main/about/about.component';
+import { ProjectComponent } from './components/main/project/project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HeroComponent },
+  {
+    path: 'main',
+    component: MainComponent,
+    children: [
+      { path: '', pathMatch: 'full', component: AboutComponent },
+      { path: 'project', component: ProjectComponent },
+    ],
+  },
+];
---
+++ src/app/components/hero/hero.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  imports: [],
+  template: `
+     <header class="bg-i">
+      <h1 class="greeter">
+        HI <br />I Am <span class="highlight">Anup Naik,</span> <br />A
+        <span class="highlight">MEAN-Stack</span> Developer
+      </h1>
+    </header>
+  `,
+  styleUrl: './hero.component.scss'
+})
+export class HeroComponent {
+
+}
